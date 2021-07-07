47ab4c71ca90e7efed8941804496fe2c30af17a4
vdtno <vaidotas.n@outlook.com>
2021-07-07 18:11:22 +0300

    formatting

--- bonus_task.groovy
+++ bonus_task.groovy
@@ -1,61 +1,61 @@
 def var_set = false
 
 pipeline {
-	agent any
-	stages {
-		stage('run stages in parallel') {
-			parallel {
-				stage('stage1 on windows') {
-					agent {
-						label "windows"
-					}
-					steps {
-						script {
-							echo "Build number is ${currentBuild.number}"
-						}
-					}
-				}
-				stage('stage2 on windows') {
-					agent {
-						label "windows"
-					}
-					steps {
-						script {
-							max = 100
-							random_num = "${Math.abs(new Random().nextInt(max))}"
-							echo "The random number: ${random_num}"
-							stash 'random_num'
-							var_set = true
-						}
-					}
-				}
-				stage('stage3 on linux') {
-					agent {
-						label "linux"
-					}
-					steps {
-						script{
-							def path_workspace = WORKSPACE
-							util = load("C:/Temp/check_free_space.groovy")
-							util.freespace(path_workspace)
-						}
-					}
-				}
-				stage('stage4 on windows') {
-					agent {
-						label "windows"
-					}
-					steps {
-						script {
-						waitUntil {
-							var_set
-						}
-							unstash 'random_num'
-							echo "The random number from stage 2: ${random_num}"
-						}
-					}
-				}
+   agent any
+   stages {
+      stage('run stages in parallel') {
+         parallel {
+            stage('stage1 on windows') {
+               agent {
+                  label "windows"
+               }
+               steps {
+                  script {
+                     echo "Build number is ${currentBuild.number}"
+                  }
+               }
             }
-		}
-	}
+            stage('stage2 on windows') {
+               agent {
+                  label "windows"
+               }
+               steps {
+                  script {
+                     max = 100
+                     random_num = "${Math.abs(new Random().nextInt(max))}"
+                     echo "The random number: ${random_num}"
+                     stash 'random_num'
+                     var_set = true
+                  }
+               }
+            }
+            stage('stage3 on linux') {
+               agent {
+                  label "linux"
+               }
+               steps {
+                  script{
+                     def path_workspace = WORKSPACE
+                     util = load("C:/Temp/check_free_space.groovy")
+                     util.freespace(path_workspace)
+                  }
+               }
+            }
+            stage('stage4 on windows') {
+               agent {
+                  label "windows"
+               }
+               steps {
+                  script {
+                  waitUntil {
+                     var_set
+                  }
+                     unstash 'random_num'
+                     echo "The random number from stage 2: ${random_num}"
+                  }
+               }
+            }
+         }
+      }
+   }
 }
--- main_task.groovy
+++ main_task.groovy
@@ -1,49 +1,49 @@
 def var_set = false  //to check if stage2 variable is already set
 
 pipeline {
-	agent any
-	stages {
-		stage('run stages in parallel') {
-			parallel {
-				stage('stage1') {
-					steps {
-						script {
-							echo "Build number is ${currentBuild.number}"
-						}
-					}
-				}
-				stage('stage2') {
-					steps {
-						script {
-							max = 100  // upper limit for random number
-							random_num = "${Math.abs(new Random().nextInt(max))}"
-							echo "The random number: ${random_num}"
-							stash 'random_num'  // save variable for stage4
-							var_set = true  // random number is generated
-						}
-					}
-				}
-				stage('stage3') {
-					steps {
-						script{
-							def path_workspace = WORKSPACE  // get workspace path
-							groovyscript = load("C:/Temp/check_free_space.groovy")  // load another groovy script
-							groovyscript.freespace(path_workspace)  // run function and pass workspace path
-						}
-					}
-				}
-				stage('stage4') {
-					steps {
-						script {
-						waitUntil {  // wait for stage2 to generate random number
-							var_set
-						}
-							unstash 'random_num'  // load variable from stage2
-							echo "The random number from stage2: ${random_num}"
-						}
-					}
-				}
-			}
-		}
-	}
+   agent any
+   stages {
+      stage('run stages in parallel') {
+         parallel {
+            stage('stage1') {
+               steps {
+                  script {
+                     echo "Build number is ${currentBuild.number}"
+                  }
+               }
+            }
+            stage('stage2') {
+               steps {
+                  script {
+                     max = 100  // upper limit for random number
+                     random_num = "${Math.abs(new Random().nextInt(max))}"
+                     echo "The random number: ${random_num}"
+                     stash 'random_num'  // save variable for stage4
+                     var_set = true  // random number is generated
+                  }
+               }
+            }
+            stage('stage3') {
+               steps {
+                  script{
+                     def path_workspace = WORKSPACE  // get workspace path
+                     groovyscript = load("C:/Temp/check_free_space.groovy")  // load another groovy script
+                     groovyscript.freespace(path_workspace)  // run function and pass workspace path
+                  }
+               }
+            }
+            stage('stage4') {
+               steps {
+                  script {
+                  waitUntil {  // wait for stage2 to generate random number
+                     var_set
+                  }
+                     unstash 'random_num'  // load variable from stage2
+                     echo "The random number from stage2: ${random_num}"
+                  }
+               }
+            }
+         }
+      }
+   }
 }
